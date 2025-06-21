c67095dd0ef9974317282e5a1696d896e2ff0211
choehs14 <yc.miraedu@gmail.com>
2025-06-21 13:09:48 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3ZjJscnI1OGZpYmNya3Z3NzFsajZhdTkxOGM4ejZvMmFteGRqbGc0ayZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/FX2pWRqziuFGZmp5nu/giphy.gif")
