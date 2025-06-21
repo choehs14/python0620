21a0f5f64fa21df20f259d59798e7d97857dc236
choehs14 <yc.miraedu@gmail.com>
2025-06-21 13:13:04 +0900

    Update 00_image.py

--- pages/00_image.py
+++ pages/00_image.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.image("https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3ZjJscnI1OGZpYmNya3Z3NzFsajZhdTkxOGM4ejZvMmFteGRqbGc0ayZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/FX2pWRqziuFGZmp5nu/giphy.gif")
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTlqZDlmYWpnemxjcHdjNzYydjYwejRsZmhmanRnb3Y2ZWczeTcxNiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3owyp2SViuDIGh8YoM/giphy.gif")
